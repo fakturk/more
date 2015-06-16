dc8a26eaeff8bfcd745dd623e44371f8f5b4868a
fakturk <f.akturk@gmail.com>
2015-06-17 03:03:20 +0900

    Working version (it is working for 4 ways)

--- app/src/main/java/com/unist/netlab/fakturk/more/MainActivity.java
+++ app/src/main/java/com/unist/netlab/fakturk/more/MainActivity.java
@@ -33,12 +33,29 @@ public class MainActivity extends ActionBarActivity {
             tv.setText(SD);
             RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(150, 50);
             layoutParams = (RelativeLayout.LayoutParams)tvMain.getLayoutParams();
-            layoutParams.leftMargin= (int) (layoutParams.leftMargin+se.values[0]);
-            layoutParams.rightMargin = (int) (layoutParams.rightMargin+se.values[0]);
-            layoutParams.topMargin = (int) (layoutParams.topMargin+se.values[1]-9.8-se.values[2]);
-            layoutParams.bottomMargin = (int) (layoutParams.bottomMargin+se.values[1]-9.8-se.values[2]);
+            //layoutParams.leftMargin= (int) (layoutParams.leftMargin+se.values[1]-9.8-se.values[2]);
+            //layoutParams.rightMargin = (int) (layoutParams.rightMargin+se.values[0]);
+            //layoutParams.topMargin = (int) (layoutParams.topMargin+se.values[1]-9.8-se.values[2]);
+            //layoutParams.bottomMargin = (int) (layoutParams.bottomMargin+se.values[1]-9.8-se.values[2]);
 
-            //tvMain.setLayoutParams(layoutParams);
+            final float alpha = (float) 0.8;
+
+            float[] gravity = new float[3];
+            gravity[0] = alpha * gravity[0] + (1 - alpha) * se.values[0];
+            gravity[1] = alpha * gravity[1] + (1 - alpha) * se.values[1];
+            gravity[2] = alpha * gravity[2] + (1 - alpha) * se.values[2];
+
+            float[] linear_acceleration = new float[3];
+            linear_acceleration[0] = se.values[0] - gravity[0];
+            linear_acceleration[1] = se.values[1] - gravity[1];
+            linear_acceleration[2] = se.values[2] - gravity[2];
+
+
+            tvMain.setLayoutParams(layoutParams);
+            tvMain.invalidate();
+
+            tvMain.setX(tvMain.getX() + linear_acceleration[0]);
+            tvMain.setY((float)(tvMain.getY()-(linear_acceleration[1]-se.values[2])));
         }
 
         @Override
@@ -52,12 +69,12 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SM = (SensorManager)getSystemService(SENSOR_SERVICE);
+        SM = (SensorManager) getSystemService(SENSOR_SERVICE);
         tv = (TextView)findViewById(R.id.textView);
         tvMain = (TextView)findViewById(R.id.textViewMain);
         buttonUp =(Button)findViewById(R.id.buttonUp);
         buttonDown = (Button)findViewById(R.id.buttonDown);
-        SM.registerListener(sL, SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),SensorManager.SENSOR_DELAY_NORMAL);
+        SM.registerListener(sL, SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_FASTEST);
 
         buttonUp.setOnClickListener(new View.OnClickListener() {
 
