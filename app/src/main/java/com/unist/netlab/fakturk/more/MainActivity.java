d775cd825976b549454a46a1fd551c575ba03c13
Fatih Akturk <f.akturk@gmail.com>
2015-06-18 17:19:29 +0900

    gyroscope

--- app/src/main/java/com/unist/netlab/fakturk/more/MainActivity.java
+++ app/src/main/java/com/unist/netlab/fakturk/more/MainActivity.java
@@ -16,11 +16,15 @@ import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import static android.util.FloatMath.cos;
+import static android.util.FloatMath.sin;
+import static android.util.FloatMath.sqrt;
+
 
 public class MainActivity extends ActionBarActivity {
 
     SensorManager SM;
-    TextView tv;
+    TextView tv, tv2;
     TextView tvMain;
     Button buttonUp, buttonDown;
 
@@ -28,17 +32,43 @@ public class MainActivity extends ActionBarActivity {
 
 
 
+
+
     SensorEventListener sL = new SensorEventListener() {
         @Override
         public void onSensorChanged(SensorEvent se) {
-            String SD="";
-            for (int i=0; i<se.values.length;i++)
+            move(se);
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
+
+    void move(SensorEvent se)
+    {
+        String SD = "";
+        int originalHeight = tvMain.getHeight();
+        int originalWidth = tvMain.getWidth();
+
+        int top = root.getTop();
+        int bottom = root.getBottom();
+        int left = root.getLeft();
+        int right = root.getRight();
+        int height = tvMain.getHeight();
+        int width = tvMain.getWidth();
+
+
+        if (se.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+
+            for (int i = 0; i < se.values.length; i++)
             {
-                SD+="values["+i+"] : "+se.values[i]+"`\n";
+                SD += "values[" + i + "] : " + se.values[i] + "`\n";
             }
             tv.setText(SD);
-            RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(150, 50);
-            layoutParams = (RelativeLayout.LayoutParams)tvMain.getLayoutParams();
+            //RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(150, 50);
+            //layoutParams = (RelativeLayout.LayoutParams) tvMain.getLayoutParams();
             //layoutParams.leftMargin= (int) (layoutParams.leftMargin+se.values[1]-9.8-se.values[2]);
             //layoutParams.rightMargin = (int) (layoutParams.rightMargin+se.values[0]);
             //layoutParams.topMargin = (int) (layoutParams.topMargin+se.values[1]-9.8-se.values[2]);
@@ -57,59 +87,113 @@ public class MainActivity extends ActionBarActivity {
             linear_acceleration[2] = se.values[2] - gravity[2];
 
 
-            tvMain.setLayoutParams(layoutParams);
-            tvMain.invalidate();
+           // tvMain.setLayoutParams(layoutParams);
+            //tvMain.invalidate();
 
-            int top = root.getTop();
-            int bottom = root.getBottom();
-            int left = root.getLeft();
-            int right = root.getRight();
-            int height = tvMain.getHeight();
-            int width = tvMain.getWidth();
 
-            if(tvMain.getX() + linear_acceleration[0]>left)
-            {
+
+          //  tvMain.setX((left+right)/2);
+          //  tvMain.setY((top+bottom)/2);
+
+            /*
+            if (tvMain.getX() + linear_acceleration[0] > left) {
                 tvMain.setX(tvMain.getX() + linear_acceleration[0]);
-            }
-            else if (tvMain.getX() + linear_acceleration[0]<=left)
-            {
+            } else if (tvMain.getX() + linear_acceleration[0] <= left) {
                 tvMain.setX(left);
             }
-            if(tvMain.getX() +width+ linear_acceleration[0]<right)
-            {
+            if (tvMain.getX() + width + linear_acceleration[0] < right) {
                 tvMain.setX(tvMain.getX() + linear_acceleration[0]);
+            } else if (tvMain.getX() + width + linear_acceleration[0] >= right) {
+                tvMain.setX(right - width);
             }
-            else if (tvMain.getX() +width+ linear_acceleration[0]>=right)
-            {
-                tvMain.setX(right-width);
-            }
-            if ((tvMain.getY()-(linear_acceleration[1]-se.values[2]))>top)
-            {
-                tvMain.setY((float)(tvMain.getY()-(linear_acceleration[1]-se.values[2])));
-            }
-            else if((tvMain.getY()-(linear_acceleration[1]-se.values[2]))<=top)
-            {
+            if ((tvMain.getY() - (linear_acceleration[1] - se.values[2])) > top) {
+                tvMain.setY((float) (tvMain.getY() - (linear_acceleration[1] - se.values[2])));
+            } else if ((tvMain.getY() - (linear_acceleration[1] - se.values[2])) <= top) {
                 tvMain.setY(top);
             }
-            if ((tvMain.getY()+height-(linear_acceleration[1]-se.values[2]))<bottom)
-            {
-                tvMain.setY((float)(tvMain.getY()-(linear_acceleration[1]-se.values[2])));
+            if ((tvMain.getY() + height - (linear_acceleration[1] - se.values[2])) < bottom) {
+                tvMain.setY((float) (tvMain.getY() - (linear_acceleration[1] - se.values[2])));
+            } else if ((tvMain.getY() + height - (linear_acceleration[1] - se.values[2])) >= bottom) {
+                tvMain.setY(bottom - height);
             }
-            else if((tvMain.getY()+height-(linear_acceleration[1]-se.values[2]))>=bottom)
-            {
-                tvMain.setY(bottom-height);
-            }
-
+            */
 
 
         }
+        if (se.sensor.getType() == Sensor.TYPE_GYROSCOPE)
+        {
+            for (int i = 0; i < se.values.length; i++)
+            {
+                SD += "values[" + (i+3) + "] : " + se.values[i] + "`\n";
+            }
 
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            tv2.setText(SD);
+             final float NS2S = 1.0f / 1000000000.0f;
+             final float[] deltaRotationVector = new float[4];
+            float timestamp = 0;
+            final double EPSILON = 0.000001;
+            if (timestamp != 0) {
+                final float dT = (se.timestamp - timestamp) * NS2S;
+                // Axis of the rotation sample, not normalized yet.
+                float axisX = se.values[0];
+                float axisY = se.values[1];
+                float axisZ = se.values[2];
 
+                // Calculate the angular speed of the sample
+                float omegaMagnitude = sqrt(axisX*axisX + axisY*axisY + axisZ*axisZ);
+
+                // Normalize the rotation vector if it's big enough to get the axis
+                if (omegaMagnitude > EPSILON) {
+                    axisX /= omegaMagnitude;
+                    axisY /= omegaMagnitude;
+                    axisZ /= omegaMagnitude;
+                }
+
+                // Integrate around this axis with the angular speed by the timestep
+                // in order to get a delta rotation from this sample over the timestep
+                // We will convert this axis-angle representation of the delta rotation
+                // into a quaternion before turning it into the rotation matrix.
+                float thetaOverTwo = omegaMagnitude * dT / 2.0f;
+                float sinThetaOverTwo = sin(thetaOverTwo);
+                float cosThetaOverTwo = cos(thetaOverTwo);
+                deltaRotationVector[0] = sinThetaOverTwo * axisX;
+                deltaRotationVector[1] = sinThetaOverTwo * axisY;
+                deltaRotationVector[2] = sinThetaOverTwo * axisZ;
+                deltaRotationVector[3] = cosThetaOverTwo;
+            }
+            timestamp = se.timestamp;
+            float[] deltaRotationMatrix = new float[9];
+            SensorManager.getRotationMatrixFromVector(deltaRotationMatrix, deltaRotationVector);
+
+            for (int i = 0; i < deltaRotationMatrix.length; i++)
+            {
+                SD += "delta [" + (i+3) + "] : " + deltaRotationMatrix[i] + "`\n";
+            }
+
+            tv2.setText(SD);
+
+            if (tvMain.getX() + se.values[1] > left) {
+                tvMain.setX(tvMain.getX() + se.values[1]);
+            } else if (tvMain.getX() + se.values[1] <= left) {
+                tvMain.setX(left);
+            }
+            if (tvMain.getX() + width + se.values[1] < right) {
+                tvMain.setX(tvMain.getX() + se.values[1]);
+            } else if (tvMain.getX() + width + se.values[1] >= right) {
+                tvMain.setX(right - width);
+            }
+            if ((tvMain.getY() - (se.values[2] )) > top) {
+                tvMain.setY((float) (tvMain.getY() - (se.values[2] )));
+            } else if ((tvMain.getY() - (se.values[2] )) <= top) {
+                tvMain.setY(top);
+            }
+            if ((tvMain.getY() + height - (se.values[2] )) < bottom) {
+                tvMain.setY((float) (se.values[2] ));
+            } else if ((tvMain.getY() + height - (se.values[2] )) >= bottom) {
+                tvMain.setY(bottom - height);
+            }
         }
-    };
-
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -117,10 +201,12 @@ public class MainActivity extends ActionBarActivity {
 
         SM = (SensorManager) getSystemService(SENSOR_SERVICE);
         tv = (TextView)findViewById(R.id.textView);
+        tv2 = (TextView)findViewById(R.id.textView2);
         tvMain = (TextView)findViewById(R.id.textViewMain);
         buttonUp =(Button)findViewById(R.id.buttonUp);
         buttonDown = (Button)findViewById(R.id.buttonDown);
         SM.registerListener(sL, SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_FASTEST);
+        SM.registerListener(sL, SM.getDefaultSensor(Sensor.TYPE_GYROSCOPE),SensorManager.SENSOR_DELAY_FASTEST);
 
 
         root = (RelativeLayout) findViewById(R.id.root);
--- app/src/main/java/com/unist/netlab/fakturk/more/SimulationView.java
+++ app/src/main/java/com/unist/netlab/fakturk/more/SimulationView.java
@@ -9,107 +9,106 @@ import android.graphics.drawable.Drawable;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.text.TextPaint;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Display;
+import android.view.LayoutInflater;
+import android.view.Surface;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 
 /**
  * TODO: document your custom view class.
  */
 public class SimulationView extends View implements SensorEventListener{
-    private String mExampleString; // TODO: use a default from R.string...
-    private int mExampleColor = Color.RED; // TODO: use a default from R.color...
-    private float mExampleDimension = 0; // TODO: use a default from R.dimen...
-    private Drawable mExampleDrawable;
 
-    private TextPaint mTextPaint;
-    private float mTextWidth;
-    private float mTextHeight;
+   // private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private Display mDisplay;
+
+    SensorManager SM;
+    TextView tv;
+    TextView tvMain;
+
+    private float mXOrigin;
+    private float mYOrigin;
+    private float mHorizontalBound;
+    private float mVerticalBound;
+
+    private float mSensorX;
+    private float mSensorY;
+    private float mSensorZ;
+    private long mSensorTimeStamp;
+
+    public float oldX, oldY;
+
+    private static final int BALL_SIZE = 32;
+    private static final int HOLE_SIZE = 40;
 
     public SimulationView(Context context) {
         super(context);
-        init(null, 0);
+
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflater.inflate(R.layout.simulation_view, null);
+        SM = (SensorManager)context.getSystemService(Context.SENSOR_SERVICE);
+        tv = (TextView)v.findViewById(R.id.textView);
+        tv.setText("deneme");
+       // tv = (TextView)getChildAt(0);
+        //tvMain = (TextView)getChildAt(1);
+        //tvMain = (TextView)findViewById(R.id.textViewMain);
+
+        SM.registerListener(this, SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),SensorManager.SENSOR_DELAY_NORMAL);
+        SM.registerListener(this, SM.getDefaultSensor(Sensor.TYPE_ORIENTATION),SensorManager.SENSOR_DELAY_GAME);
+
+        WindowManager mWindowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        mDisplay = mWindowManager.getDefaultDisplay();
+        //init();
+
     }
 
-    public SimulationView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        init(attrs, 0);
+    public SimulationView(Context context, View view)
+    {
+        super(context, (AttributeSet) view);
+        init(view);
     }
 
-    public SimulationView(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-        init(attrs, defStyle);
+    public void init(View view)
+    {
+        tv = (TextView)view.findViewById(R.id.textView);
+        //tv = (TextView)getChildAt(0);
+        //tvMain = (TextView)getChildAt(1);
+        //tvMain = (TextView)findViewById(R.id.textViewMain);
+        tv.setText("deneme");
     }
 
-    private void init(AttributeSet attrs, int defStyle) {
-        // Load attributes
-        final TypedArray a = getContext().obtainStyledAttributes(
-                attrs, R.styleable.SimulationView, defStyle, 0);
 
-        mExampleString = a.getString(
-                R.styleable.SimulationView_exampleString);
-        mExampleColor = a.getColor(
-                R.styleable.SimulationView_exampleColor,
-                mExampleColor);
-        // Use getDimensionPixelSize or getDimensionPixelOffset when dealing with
-        // values that should fall on pixel boundaries.
-        mExampleDimension = a.getDimension(
-                R.styleable.SimulationView_exampleDimension,
-                mExampleDimension);
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
 
-        if (a.hasValue(R.styleable.SimulationView_exampleDrawable)) {
-            mExampleDrawable = a.getDrawable(
-                    R.styleable.SimulationView_exampleDrawable);
-            mExampleDrawable.setCallback(this);
-        }
-
-        a.recycle();
-
-        // Set up a default TextPaint object
-        mTextPaint = new TextPaint();
-        mTextPaint.setFlags(Paint.ANTI_ALIAS_FLAG);
-        mTextPaint.setTextAlign(Paint.Align.LEFT);
-
-        // Update TextPaint and text measurements from attributes
-        invalidateTextPaintAndMeasurements();
     }
 
-    private void invalidateTextPaintAndMeasurements() {
-        mTextPaint.setTextSize(mExampleDimension);
-        mTextPaint.setColor(mExampleColor);
-        mTextWidth = mTextPaint.measureText(mExampleString);
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        mXOrigin = w * 0.5f;
+        mYOrigin = h * 0.5f;
 
-        Paint.FontMetrics fontMetrics = mTextPaint.getFontMetrics();
-        mTextHeight = fontMetrics.bottom;
+        mHorizontalBound = (w - BALL_SIZE) * 0.5f;
+        mVerticalBound = (h - BALL_SIZE) * 0.5f;
     }
+
+
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        // TODO: consider storing these as member variables to reduce
-        // allocations per draw cycle.
-        int paddingLeft = getPaddingLeft();
-        int paddingTop = getPaddingTop();
-        int paddingRight = getPaddingRight();
-        int paddingBottom = getPaddingBottom();
 
-        int contentWidth = getWidth() - paddingLeft - paddingRight;
-        int contentHeight = getHeight() - paddingTop - paddingBottom;
-
-        // Draw the text.
-        canvas.drawText(mExampleString,
-                paddingLeft + (contentWidth - mTextWidth) / 2,
-                paddingTop + (contentHeight + mTextHeight) / 2,
-                mTextPaint);
-
-        // Draw the example drawable on top of the text.
-        if (mExampleDrawable != null) {
-            mExampleDrawable.setBounds(paddingLeft, paddingTop,
-                    paddingLeft + contentWidth, paddingTop + contentHeight);
-            mExampleDrawable.draw(canvas);
-        }
     }
 
     /**
@@ -117,82 +116,59 @@ public class SimulationView extends View implements SensorEventListener{
      *
      * @return The example string attribute value.
      */
-    public String getExampleString() {
-        return mExampleString;
-    }
 
-    /**
-     * Sets the view's example string attribute value. In the example view, this string
-     * is the text to draw.
-     *
-     * @param exampleString The example string attribute value to use.
-     */
-    public void setExampleString(String exampleString) {
-        mExampleString = exampleString;
-        invalidateTextPaintAndMeasurements();
-    }
-
-    /**
-     * Gets the example color attribute value.
-     *
-     * @return The example color attribute value.
-     */
-    public int getExampleColor() {
-        return mExampleColor;
-    }
-
-    /**
-     * Sets the view's example color attribute value. In the example view, this color
-     * is the font color.
-     *
-     * @param exampleColor The example color attribute value to use.
-     */
-    public void setExampleColor(int exampleColor) {
-        mExampleColor = exampleColor;
-        invalidateTextPaintAndMeasurements();
-    }
-
-    /**
-     * Gets the example dimension attribute value.
-     *
-     * @return The example dimension attribute value.
-     */
-    public float getExampleDimension() {
-        return mExampleDimension;
-    }
-
-    /**
-     * Sets the view's example dimension attribute value. In the example view, this dimension
-     * is the font size.
-     *
-     * @param exampleDimension The example dimension attribute value to use.
-     */
-    public void setExampleDimension(float exampleDimension) {
-        mExampleDimension = exampleDimension;
-        invalidateTextPaintAndMeasurements();
-    }
-
-    /**
-     * Gets the example drawable attribute value.
-     *
-     * @return The example drawable attribute value.
-     */
-    public Drawable getExampleDrawable() {
-        return mExampleDrawable;
-    }
-
-    /**
-     * Sets the view's example drawable attribute value. In the example view, this drawable is
-     * drawn above the text.
-     *
-     * @param exampleDrawable The example drawable attribute value to use.
-     */
-    public void setExampleDrawable(Drawable exampleDrawable) {
-        mExampleDrawable = exampleDrawable;
-    }
 
     @Override
-    public void onSensorChanged(SensorEvent event) {
+    public void onSensorChanged(SensorEvent se) {
+        if (se.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+        {
+
+            Log.v("fakturk", "x: " + se.values[0] + ", y: " + se.values[1] + " , z: " + se.values[2]);
+            switch (mDisplay.getRotation()) {
+                case Surface.ROTATION_0:
+                    mSensorX = se.values[0];
+                    mSensorY = se.values[1];
+                    break;
+                case Surface.ROTATION_90:
+                    mSensorX = -se.values[1];
+                    mSensorY = se.values[0];
+                    break;
+                case Surface.ROTATION_180:
+                    mSensorX = -se.values[0];
+                    mSensorY = -se.values[1];
+                    break;
+                case Surface.ROTATION_270:
+                    mSensorX = se.values[1];
+                    mSensorY = -se.values[0];
+                    break;
+            }
+            mSensorZ = se.values[2];
+            mSensorTimeStamp = se.timestamp;
+//            Particle corner = new Particle();
+           // corner.updatePosition(mSensorX, mSensorY, mSensorZ, mSensorTimeStamp);
+            //corner.resolveCollisionWithBounds(mHorizontalBound, mVerticalBound);
+            String SD = "";
+            for (int i = 0; i < se.values.length; i++)
+            {
+               // SD += "values[" + i + "] : " + se.values[i] +"\n";
+            }
+            //SD +="mPosX : "+corner.mPosX+"\n";
+           // SD +="mPosY : "+corner.mPosY+"\n";
+
+
+
+            tv.setText(SD);
+            RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(150, 50);
+           // layoutParams = (RelativeLayout.LayoutParams) tvMain.getLayoutParams();
+            //layoutParams.leftMargin = (int) (layoutParams.leftMargin - (se.values[1]-oldX));
+            //layoutParams.rightMargin = -250;
+            //layoutParams.topMargin = (int) (layoutParams.topMargin - (se.values[2]-oldY));
+            //layoutParams.bottomMargin = -250;
+
+           // tvMain.setLayoutParams(layoutParams);
+            oldX = (int) (se.values[1]);
+            oldY = (int) (se.values[2]);
+        }
 
     }
 
