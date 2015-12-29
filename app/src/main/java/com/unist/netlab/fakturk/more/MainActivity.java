4a5faccaf836f4541402005f298b34283014c774
fakturk <f.akturk@gmail.com>
2015-12-30 03:13:54 +0900

    SensorService added as a broadcast

--- app/src/main/java/com/unist/netlab/fakturk/more/MainActivity.java
+++ app/src/main/java/com/unist/netlab/fakturk/more/MainActivity.java
@@ -2,9 +2,11 @@ package com.unist.netlab.fakturk.more;
 
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -13,7 +15,9 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.RemoteException;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -34,6 +38,10 @@ public class MainActivity extends Activity implements SensorEventListener
 
     RelativeLayout root;
 
+    MyReceiver myReceiver=null;
+    Intent i;
+    static final String LOG_TAG = "ServiceActivity";
+
 
      @Override
     public void onSensorChanged(SensorEvent se)
@@ -53,13 +61,16 @@ public class MainActivity extends Activity implements SensorEventListener
 
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        Log.d( LOG_TAG, "onCreate" );
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        i= new Intent(this, SensorService.class);
+        Log.d(LOG_TAG, "onCreate/startService");
 
         //SM = (SensorManager) getSystemService(SENSOR_SERVICE);
         tv = (TextView)  findViewById(R.id.textView);
@@ -73,8 +84,26 @@ public class MainActivity extends Activity implements SensorEventListener
         buttonStopService.setEnabled(false);
         buttonFunc.setEnabled(false);
 
+        LocalBroadcastManager.getInstance(this).registerReceiver(
+                new BroadcastReceiver() {
+                    @Override
+                    public void onReceive(Context context, Intent intent) {
+                       // double latitude = intent.getDoubleExtra(LocationBroadcastService.EXTRA_LATITUDE, 0);
+                       // double longitude = intent.getDoubleExtra(LocationBroadcastService.EXTRA_LONGITUDE, 0);
+                        tv.setText("ACC : \n"+intent.getStringExtra("ACC")+
+                                        "\n"+
+                                "GYR : \n"+intent.getStringExtra("GYR")+
+                                         "\n"+
+                                "GRA : \n"+intent.getStringExtra("GRA"));
+                        //callFunc();
+                    }
+                }, new IntentFilter(SensorService.ACTION_SENSOR_BROADCAST)
+        );
+
         //SM.registerListener(sL, SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_FASTEST);
         //SM.registerListener(sL, SM.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_GAME);
+
+
 
 
 
@@ -143,6 +172,19 @@ public class MainActivity extends Activity implements SensorEventListener
 
 
 
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopService(new Intent(this, SensorService.class));
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startService(new Intent(this, SensorService.class));
     }
 
 
@@ -217,6 +259,16 @@ public class MainActivity extends Activity implements SensorEventListener
     }
 
 
+
+}
+ class MyReceiver extends BroadcastReceiver{
+    static final String Log_Tag = "MyReceiver";
+    @Override
+    public void onReceive(Context arg0, Intent arg1){
+       // Log.d(LOG_TAG, "onReceive");
+        String measurement = arg1.getStringExtra("measurement");
+        System.out.println("I am here");
+    }
 
 }
 
--- app/src/main/java/com/unist/netlab/fakturk/more/SensorService.java
+++ app/src/main/java/com/unist/netlab/fakturk/more/SensorService.java
@@ -10,14 +10,32 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.AsyncTask;
+import android.os.Environment;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 import android.widget.Toast;
+
+import com.opencsv.CSVWriter;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class SensorService extends Service
 {
+    CSVWriter writer = null;
+
     private String text, text_acc="not catched\n", text_gyr="not catched\n", text_gra="not catched\n";
     SensorManager SM;
+
+    public static final String ACTION_SENSOR_BROADCAST = SensorService.class.getName() + "SensorBroadcast";
+
+    static final String LOG_TAG = "SimpleService";
+
+    Intent intent = new Intent(SensorService.this, MainActivity.class);
+
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId)
@@ -28,38 +46,41 @@ public class SensorService extends Service
        return super.onStartCommand(intent, flags, startId);
     }
 
-    SensorEventListener sL = new SensorEventListener() {
-        @Override
-        public void onAccuracyChanged(Sensor arg0, int arg1) {
-        }
-        @Override
-        public void onSensorChanged(SensorEvent se) {
-
-            //Intent bIntent = new Intent(SensorService.this, MainActivity.class);
-            new SensorEventLoggerTask().execute(se);
-            switch(se.sensor.getType()){
-                case Sensor.TYPE_ACCELEROMETER :
-                    text_acc = "";
-                    text_acc += "X = " + se.values[0] + "\n";
-                    text_acc += "Y = " + se.values[1] + "\n";
-                    text_acc += "Z = " + se.values[2] + "\n";
-                    //Change the text of notification to accelerometer using setContetnText
-                    //If you change the notification then you notify the notification once again
-                    //When you use notify() function ID is must be same to precious one
-
-                case Sensor.TYPE_GYROSCOPE :
-                    text_gyr = "";
-                    text_gyr += "X = " + se.values[0] + "\n";
-                    text_gyr += "Y = " + se.values[1] + "\n";
-                    text_gyr += "Z = " + se.values[2] + "\n";
-                case Sensor.TYPE_GRAVITY :
-                    text_gra = "";
-                    text_gra += "X = " + se.values[0] + "\n";
-                    text_gra += "Y = " + se.values[1] + "\n";
-                    text_gra += "Z = " + se.values[2] + "\n";
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Toast.makeText(this, "Service Created", Toast.LENGTH_LONG).show();
+        Log.d(LOG_TAG, "onStartCommand");
+        SM = (SensorManager)getSystemService(SENSOR_SERVICE);
+        SensorEventListener sL = new SensorEventListener() {
+            @Override
+            public void onAccuracyChanged(Sensor arg0, int arg1) {
             }
-        }
-    };
+            @Override
+            public void onSensorChanged(SensorEvent se) {
+                Log.d(LOG_TAG, "onSensorChanged");
+
+                sendBroadcastMessage(se);
+
+
+
+                //Intent bIntent = new Intent(SensorService.this, MainActivity.class);
+                //new SensorEventLoggerTask().execute(se);
+
+
+
+               // intent.putExtra("measurement",text);
+                //sendBroadcast(intent);
+            }
+        };
+        SM.registerListener(sL, SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+        SM.registerListener(sL, SM.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_NORMAL);
+        SM.registerListener(sL, SM.getDefaultSensor(Sensor.TYPE_GRAVITY), SensorManager.SENSOR_DELAY_NORMAL);
+
+
+    }
+
+
     NotificationCompat.Builder mCompatBuilder;
     NotificationManager nm;
     Intent notiIntent;
@@ -68,12 +89,15 @@ public class SensorService extends Service
     @Override
     public IBinder onBind(Intent intent)
     {
+        /*
         Toast.makeText(this, "onBind - Service Started", Toast.LENGTH_LONG).show();
 
         SM = (SensorManager)getSystemService(SENSOR_SERVICE);
         SM.registerListener(sL, SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
         SM.registerListener(sL, SM.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_NORMAL);
         SM.registerListener(sL, SM.getDefaultSensor(Sensor.TYPE_GRAVITY), SensorManager.SENSOR_DELAY_NORMAL);
+
+
 
         //nm.notify(333, mCompatBuilder.build());
 
@@ -92,6 +116,9 @@ public class SensorService extends Service
         this.startForeground(1, barNotif);
         return mBinder;
 
+        */
+        return null;
+
 
     }
     private final AIDLService.Stub mBinder = new AIDLService.Stub() {
@@ -107,15 +134,62 @@ public class SensorService extends Service
     };
 
 
-    @Override
-    public void onCreate() {
-        Toast.makeText(this, "Service Created", Toast.LENGTH_LONG).show();
-        super.onCreate();
+    private void sendBroadcastMessage(SensorEvent se) {
+        if (se != null) {
+            Intent intent = new Intent(ACTION_SENSOR_BROADCAST);
+            String[] entries = new String[9];
+            switch(se.sensor.getType()){
+                case Sensor.TYPE_ACCELEROMETER :
+                    text_acc = "";
+                    text_acc += "X = " + se.values[0] + "\n";
+                    text_acc += "Y = " + se.values[1] + "\n";
+                    text_acc += "Z = " + se.values[2] + "\n";
+                    entries[0] = String.valueOf(se.values[0]);
+                    entries[1] = String.valueOf(se.values[1]);
+                    entries[2] = String.valueOf(se.values[2]);
+
+                    //Change the text of notification to accelerometer using setContetnText
+                    //If you change the notification then you notify the notification once again
+                    //When you use notify() function ID is must be same to precious one
+
+                case Sensor.TYPE_GYROSCOPE :
+                    text_gyr = "";
+                    text_gyr += "X = " + se.values[0] + "\n";
+                    text_gyr += "Y = " + se.values[1] + "\n";
+                    text_gyr += "Z = " + se.values[2] + "\n";
+                    entries[3] = String.valueOf(se.values[0]);
+                    entries[4] = String.valueOf(se.values[1]);
+                    entries[5] = String.valueOf(se.values[2]);
+                case Sensor.TYPE_GRAVITY :
+                    text_gra = "";
+                    text_gra += "X = " + se.values[0] + "\n";
+                    text_gra += "Y = " + se.values[1] + "\n";
+                    text_gra += "Z = " + se.values[2] + "\n";
+                    entries[6] = String.valueOf(se.values[0]);
+                    entries[7] = String.valueOf(se.values[1]);
+                    entries[8] = String.valueOf(se.values[2]);
+            }
+            intent.putExtra("ACC", text_acc);
+            intent.putExtra("GYR", text_gyr);
+            intent.putExtra("GRA", text_gra);
+
+            try {
+                writer = new CSVWriter(new FileWriter(Environment.getExternalStorageDirectory().getAbsolutePath()+ File.separator+"SensorData.csv"),',');
+                writer.writeNext(entries);
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+            LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+        }
     }
+
 
     @Override
     public boolean onUnbind(Intent intent){
-        SM.unregisterListener(sL);
+        //SM.unregisterListener(sL);
 //		nm.cancel(222);
         super.onUnbind(intent);
         return true;
@@ -124,7 +198,8 @@ public class SensorService extends Service
     public void onDestroy(){
 
         Toast.makeText(this, "Service Stopped", Toast.LENGTH_LONG).show();
-        SM.unregisterListener(sL);
+        Log.d( LOG_TAG, "onDestroy" );
+        //SM.unregisterListener(sL);
         //nm.cancel(333);
         super.onDestroy();
     }
@@ -141,3 +216,4 @@ class SensorEventLoggerTask extends
         return null;
     }
 }
+
