389269f96a9a94610d3346a8f61959650faf5eb7
Fatih Akturk <f.akturk@gmail.com>
2015-12-29 11:43:26 +0900

    Move class created

--- app/src/main/java/com/unist/netlab/fakturk/more/MainActivity.java
+++ app/src/main/java/com/unist/netlab/fakturk/more/MainActivity.java
@@ -1,80 +1,141 @@
 package com.unist.netlab.fakturk.more;
 
+
+import android.app.Activity;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity
+{
 
     SensorManager SM;
-    TextView tv;
+    TextView tv, tv2;
     TextView tvMain;
+    Button buttonUp, buttonDown, buttonStopService, buttonStartService;
+    Move move;
 
-    SensorEventListener sL = new SensorEventListener() {
+
+    RelativeLayout root;
+
+
+    SensorEventListener sL = new SensorEventListener()
+    {
         @Override
-        public void onSensorChanged(SensorEvent se) {
-            String SD="";
-            for (int i=0; i<se.values.length;i++)
-            {
-                SD+="values["+i+"] : "+se.values[i]+"`\n";
-            }
-            tv.setText(SD);
-            RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(150, 50);
-            layoutParams = (RelativeLayout.LayoutParams)tvMain.getLayoutParams();
-            layoutParams.leftMargin= (int) (layoutParams.leftMargin+se.values[0]);
-            layoutParams.rightMargin = (int) (layoutParams.rightMargin+se.values[0]);
-            layoutParams.topMargin = (int) (layoutParams.topMargin+se.values[2]);
-            layoutParams.bottomMargin = (int) (layoutParams.bottomMargin+se.values[2]);
+        public void onSensorChanged(SensorEvent se)
+        {
 
-            tvMain.setLayoutParams(layoutParams);
+            move = new Move(se,  tv,  tv2,   tvMain,  root);
+            move.moveIt();
+
         }
 
         @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        public void onAccuracyChanged(Sensor sensor, int accuracy)
+        {
 
         }
     };
 
+
+
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SM = (SensorManager)getSystemService(SENSOR_SERVICE);
-        tv = (TextView)findViewById(R.id.textView);
-        tvMain = (TextView)findViewById(R.id.textViewMain);
-        SM.registerListener(sL, SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),SensorManager.SENSOR_DELAY_NORMAL);
+        SM = (SensorManager) getSystemService(SENSOR_SERVICE);
+        tv = (TextView)  findViewById(R.id.textView);
+        tv2 = (TextView) findViewById(R.id.textView2);
+        tvMain = (TextView) findViewById(R.id.textViewMain);
+        buttonUp = (Button) findViewById(R.id.buttonSizeUp);
+        buttonDown = (Button) findViewById(R.id.buttonSizeDown);
+        //buttonStartService = (Button) findViewById(R.id.buttonStartSErvice);
+        //buttonStopService = (Button) findViewById(R.id.buttonStopService);
+
+        SM.registerListener(sL, SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_FASTEST);
+        SM.registerListener(sL, SM.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_GAME);
+
+
+        root = (RelativeLayout) findViewById(R.id.root);
+
+        buttonUp.setOnClickListener(new View.OnClickListener()
+        {
+
+
+            @Override
+            public void onClick(View v)
+            {
+
+                DisplayMetrics metrics;
+                metrics = getApplicationContext().getResources().getDisplayMetrics();
+                float Textsize = tv.getTextSize() / metrics.density;
+                tvMain.setTextSize(Textsize * 2);
+            }
+        });
+
+        buttonDown.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+                DisplayMetrics metrics;
+                metrics = getApplicationContext().getResources().getDisplayMetrics();
+                float Textsize = tv.getTextSize() / metrics.density;
+                tvMain.setTextSize(Textsize / 2);
+            }
+        });
+
+
 
     }
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
+        {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
 }
+
+
+
+
+
